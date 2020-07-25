afe63cbe89c49113b6244fb89516d7844710c7bc
glenn <glenn.ludszuweit@gmail.com>
2020-07-25 10:16:25 +0200

    navbar javascript classList.toggle

--- navbar/app.js
+++ navbar/app.js
@@ -1,10 +1,11 @@
-const toggle = document.querySelector('.nav-toggle');
+const toggler = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
-toggle.addEventListener('click', function () {
-  if (links.classList.contains('show-links')) {
-    links.classList.remove('show-links');
-  } else {
-    links.classList.add('show-links');
-  }
+toggler.addEventListener('click', function () {
+  //   if (links.classList.contains('show-links')) {
+  //     links.classList.remove('show-links');
+  //   } else {
+  //     links.classList.add('show-links');
+  //   }
+  links.classList.toggle('show-links');
 });
